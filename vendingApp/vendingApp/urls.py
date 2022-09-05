85f0c9bb3a6fb380c69db02b948fb0687b632d13
RoboEndure2601 <parththesiya@gmail.com>
2022-09-05 16:11:04 +0530

    test model 001

--- vendingApp/vendingApp/urls.py
+++ vendingApp/vendingApp/urls.py
@@ -1,7 +1,7 @@
-"""vendingApp URL Configuration
+"""test_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+
+from django.conf.urls import url
+from test_1 import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$',views.index,name='index'),
+    url(r'^quantity/',views.quantity,name='quantity'),
+
+
+
 ]
